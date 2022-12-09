3fa36fb2de903d0c993d34c99b6f62277e1d2002
Sadek-Hossain-Nishat <sadekhossainnishat@gmail.com>
2022-12-09 01:51:55 -0800

    third commit

--- app/src/main/java/com/example/imageanimationsandroidapp/MainActivity.java
+++ app/src/main/java/com/example/imageanimationsandroidapp/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.imageanimationsandroidapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+
 public class MainActivity extends AppCompatActivity {
+    private BottomSheetBehavior mBottomSheetBehavior;
+    private TextView mTextViewState;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View bottomSheet = findViewById(R.id.id_bottom_sheet);
+        mBottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
     }
 }
