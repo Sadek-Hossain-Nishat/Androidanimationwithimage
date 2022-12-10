09618e5efc6b8eef57027c242910870094d0ce57
Sadek-Hossain-Nishat <sadekhossainnishat@gmail.com>
2022-12-09 20:21:54 -0800

    fourth commit

--- app/src/main/java/com/example/imageanimationsandroidapp/MainActivity.java
+++ app/src/main/java/com/example/imageanimationsandroidapp/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.imageanimationsandroidapp;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -19,5 +21,35 @@ public class MainActivity extends AppCompatActivity {
 
         View bottomSheet = findViewById(R.id.id_bottom_sheet);
         mBottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+        mTextViewState = findViewById(R.id.id_text_view_state);
+        Button buttonExpand =  findViewById(R.id.id_button_expand);
+        Button buttonCollapse = findViewById(R.id.id_button_collapse);
+
+        buttonExpand.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+
+            }
+        });
+        buttonCollapse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+
+            }
+        });
+
+        mBottomSheetBehavior.addBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+            }
+        });
     }
 }
