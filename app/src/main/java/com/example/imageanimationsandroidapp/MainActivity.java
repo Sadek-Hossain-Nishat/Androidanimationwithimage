39aef7cca02f014fc8bb2733dfc7a554055384fd
Sadek-Hossain-Nishat <sadekhossainnishat@gmail.com>
2022-12-13 18:46:16 -0800

    sixth commit

--- app/src/main/java/com/example/imageanimationsandroidapp/MainActivity.java
+++ app/src/main/java/com/example/imageanimationsandroidapp/MainActivity.java
@@ -2,76 +2,35 @@ package com.example.imageanimationsandroidapp;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
-import androidx.annotation.NonNull;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
 
 public class MainActivity extends AppCompatActivity {
-    private BottomSheetBehavior mBottomSheetBehavior;
-    private TextView mTextViewState;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        View bottomSheet = findViewById(R.id.id_bottom_sheet);
-        mBottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
-        mTextViewState = findViewById(R.id.id_text_view_state);
-        Button buttonExpand =  findViewById(R.id.id_button_expand);
-        Button buttonCollapse = findViewById(R.id.id_button_collapse);
-
-        buttonExpand.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton fab = findViewById(R.id.id_fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
 
-            }
-        });
-        buttonCollapse.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+
+                Snackbar.make(view,"This is a Snackbar",Snackbar.LENGTH_LONG).show();
 
             }
         });
 
-        mBottomSheetBehavior.addBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
-            @Override
-            public void onStateChanged(@NonNull View bottomSheet, int newState) {
-
-                switch (newState) {
-                    case BottomSheetBehavior.STATE_COLLAPSED:
-                        mTextViewState.setText("Collapsed");
-                        break;
-                    case BottomSheetBehavior.STATE_DRAGGING:
-                        mTextViewState.setText("Dragging...");
-                        break;
-                    case BottomSheetBehavior.STATE_EXPANDED:
-                        mTextViewState.setText("Expanded");
-                        break;
-                    case BottomSheetBehavior.STATE_HIDDEN:
-                        mTextViewState.setText("Hidden");
-                        break;
-                    case BottomSheetBehavior.STATE_SETTLING:
-                        mTextViewState.setText("Settling...");
-                        break;
 
 
-                }
 
-            }
-
-            @Override
-            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
-
-                mTextViewState.setText("Sliding...");
-
-            }
-        });
     }
 }
