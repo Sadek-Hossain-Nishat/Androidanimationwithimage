97c81bc9306c0adec55bf764c373ae7d2dca537b
Sadek-Hossain-Nishat <sadekhossainnishat@gmail.com>
2022-12-15 20:33:07 -0800

    seventh commit

--- app/src/main/java/com/example/imageanimationsandroidapp/MainActivity.java
+++ app/src/main/java/com/example/imageanimationsandroidapp/MainActivity.java
@@ -2,21 +2,27 @@ package com.example.imageanimationsandroidapp;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 
 public class MainActivity extends AppCompatActivity {
+    private BottomSheetBehavior bottomSheetBehavior;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View  bottomSheet = findViewById(R.id.id_bottom_sheet);
+        bottomSheetBehavior  = BottomSheetBehavior.from(bottomSheet);
 
         FloatingActionButton fab = findViewById(R.id.id_fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -28,6 +34,15 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        Button button = findViewById(R.id.id_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+            }
+        });
+
 
 
 
