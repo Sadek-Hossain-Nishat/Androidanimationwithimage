7091f1bea8b14bbd48de16a7c289aea88b172379
Sadek Hossain Nishat <65479688+Sadek-Hossain-Nishat@users.noreply.github.com>
2023-01-06 19:21:49 -0800

    twentyfirst commit

--- app/src/main/java/com/example/imageanimationsandroidapp/MainActivity.java
+++ app/src/main/java/com/example/imageanimationsandroidapp/MainActivity.java
@@ -2,18 +2,21 @@ package com.example.imageanimationsandroidapp;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.transition.Transition;
 import android.transition.TransitionManager;
 import android.view.View;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.constraintlayout.widget.Placeholder;
-
-
-import java.util.Random;
+import androidx.constraintlayout.widget.ConstraintSet;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private ConstraintLayout layout;
+    private ConstraintSet constraintSetOld = new ConstraintSet();
+    private ConstraintSet constraintSetNew = new ConstraintSet();
+    private boolean altLayout;
 
 
 
@@ -25,6 +28,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        layout = findViewById(R.id.layout);
+
+        constraintSetOld.clone(layout);
+
+        constraintSetNew.clone(this,R.layout.activity_main_alt);
+
+
+
+
+
 
 
 
@@ -32,6 +45,20 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
+    public void swapView(View view) {
+
+        TransitionManager.beginDelayedTransition(layout);
+        if (!altLayout) {
+
+            constraintSetNew.applyTo(layout);
+            altLayout = true;
+        } else {
+            
+        }
+
+
+    }
 
 
 }
